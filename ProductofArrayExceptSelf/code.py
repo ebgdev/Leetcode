d265acd3de0c65aef76e3f34a6c1eaca54c5d9ec
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-08-19 20:01:31 +0300

    ProductofArrayExceptSelf get updated.

--- ProductofArrayExceptSelf/code.py
+++ ProductofArrayExceptSelf/code.py
@@ -1,7 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        new_arr = []
-        for i in range(len(nums)):
-	        arr = nums[:i]+nums[i+1:]
-	        new_arr.append(reduce(lambda x, y: x*y, arr))
-        return new_arr
+        n = len(nums)
+        left_products = [1] * n
+        right_products = [1] * n
+        result = [1] * n
+        
+        left_product = 1
+        for i in range(n):
+            left_products[i] = left_product
+            left_product *= nums[i]
+        
+        right_product = 1
+        for i in range(n - 1, -1, -1):
+            right_products[i] = right_product
+            right_product *= nums[i]
+        
+        for i in range(n):
+            result[i] = left_products[i] * right_products[i]
+        
+        return result
