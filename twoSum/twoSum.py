db3e00e226c2295f26cd0c81c12448b6dc2c135e
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2023-10-22 15:22:55 +0300

    twoSum get updated with 2 another algoritms and one deleted.

--- twoSum/twoSum.py
+++ twoSum/twoSum.py
@@ -1,4 +1,59 @@
-##################### 1 #########################
+from typing import List
+
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+
+# -----------------1
+
+
+
+# class Solution:
+#   def twoSum(self, nums: List[int], target: int) -> List[int]:
+#       my_hash = {}
+#       length = len(nums)
+#       for i in range(length):
+#           my_hash[nums[i]] = i
+
+#           for i in range(length):
+#               complement = target - nums[i]
+#               if complement in my_hash and my_hash[complement]!= i:
+#                   return [i,my_hash[complement]]
+
+
+
+
+# nums = [2,7,11,15]
+# target = 9
+
+# my_obj = Solution()
+# print(my_obj.twoSum(nums,target))
+
+# -----------------2
+
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_hash = {}
+        length = len(nums)
+
+        for i,num in enumerate(nums):
+            diff = target - num
+            if diff in my_hash:
+                return [my_hash[diff],i]
+            my_hash[num] = i    
+
+
+nums = [2,7,11,15]
+target = 9
+
+my_obj = Solution()
+print(my_obj.twoSum(nums,target))
+
+
+
+##################### 3 #########################
 class Solution:
     #O(n^2)
     def twoSum(self, nums, target):
@@ -12,30 +67,6 @@ target = 6
 nums = [3, 2, 4]
 deneme = Solution()
 print(deneme.twoSum(nums, target))
-
-##################### 2 #########################
-
-class Solution:
-    #O(1)
-    def twoSum(self, nums, target):
-        d = {}
-        for index, val in enumerate(nums):
-            rem = target - val
-            if rem in d:return [d[rem], index]
-            d[val] = index
-
-
-target = 6
-nums = [3, 2, 4]
-
-deneme = Solution()
-print(deneme.twoSum(nums, target))
-
-
-
-
-
-
 
 
 
